3097d329e9c4d222703eb6e16e14771d5be50b8e
Samleung <callsamleung@gmail.com>
2017-02-24 21:57:49 +0800

    apply autopep8

--- sony_playlist.py
+++ sony_playlist.py
@@ -7,6 +7,7 @@ DETERMINE_EXT = DETERMINE_EXT + map(str.upper, DETERMINE_EXT)
 
 
 class SonyPlayList(object):
+
     def __init__(self, _location, _save_dir):
         self.location = _location
         self.save_dir = _save_dir
@@ -29,7 +30,8 @@ class SonyPlayList(object):
             fs.writelines(contents)
 
     def build_playlist(self, _top_dir, _dir=None):
-        if not _dir: _dir = self.location
+        if not _dir:
+            _dir = self.location
         for (dirpath, dirnames, filenames) in os.walk(_dir):
             contents = []
             relative_path = dirpath[dirpath.rindex(_top_dir):]
@@ -38,17 +40,17 @@ class SonyPlayList(object):
                     filename = self._m3u8_name(relative_path)
                     music_path = self._window_slant(self._music_path(relative_path, _file))
                     contents.append('{}\n'.format(music_path))
-            if contents: self._save_m3u8(filename, contents)
-                
+            if contents:
+                self._save_m3u8(filename, contents)
+
 import argparse
-DOC= '''Create Playlist for Sony Media player'''
+DOC = '''Create Playlist for Sony Media player'''
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DOC)
     parser.add_argument('src', type=str, help='Source Directory')
     parser.add_argument('dest', type=str, help='Playlist Save Directory')
-    args =parser.parse_args()
+    args = parser.parse_args()
     _dir = args.src
     sp = SonyPlayList(_dir, args.dest)
     sp.build_playlist(os.path.split(_dir)[-1])
-
